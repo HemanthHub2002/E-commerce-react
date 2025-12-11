c7d047129ba1b43dfcc52c1ec5674956de25a7e8
HemanthHub2002 <hemanthkgr2002@gmail.com>
2025-12-11 16:55:46 +0530

    first commit

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+        <footer className="fixed-bottom w-100 bg-dark text-warning text-center">
+            &copy; 2025
+        </footer>
+        </>
+    )
+}
